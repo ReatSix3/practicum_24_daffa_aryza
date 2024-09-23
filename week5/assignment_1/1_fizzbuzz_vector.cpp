fea6fa0d434a0d53f9a76052eb9762051c9ecf25
Daffa Aryza <daffapasya1906@gmail.com>
2024-09-23 15:17:52 +0700

    4th commitment

--- week5/assignment_1/1_fizzbuzz_vector.cpp
+++ week5/assignment_1/1_fizzbuzz_vector.cpp
@@ -6,25 +6,19 @@ int main(){
     std::vector<std::string> output{};
     for(int i = 1;i<=100;i++){
         if(i % 15 == 0){
-            std::cout<<"FizzBuzz ";
             output.push_back("FizzBuzz");
         }
         else if(i % 5 == 0){
-            std::cout<<"Buzz ";
             output.push_back("Buzz");
         }
         else if(i % 3 == 0){
-            std::cout<<"Fizz ";
             output.push_back("Fizz");
         }
         else{
             std::string i_string = std::to_string(i);
-            std::cout<<i_string<<" ";
             output.push_back(i_string);
         }
     }
-    //int len_output = std::ssize(output);
-    //std::cout<<" LOL : "<<len_output;
     std::cout<<"\n \n{";
     for(int x = 0;x<100;x += 1){
         std::cout<<output[x];
@@ -33,6 +27,5 @@ int main(){
     }
     }
     std::cout<<"}";
-    //std::cout<<output;
     return 0;
     }   
